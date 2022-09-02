ac47fbc14c7349314e76fbb2560ee23ce878fb2b
ulisescorrales <ulisescorralesallen@gmail.com>
2022-09-02 13:38:49 -0300

    Agregado arreglo Sumador[]

---
+++ src/tpoarreglo/Sumador.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpoarreglo;
+
+/**
+ *
+ * @author ulises.corrales
+ */
+class Sumador extends Thread {
+
+    private final int inicio;
+    private final int fin;
+    private final int[] arreglo;
+    private int suma = 0;
+
+    //Construye un nuevo hilo.
+    public Sumador(int inicio, int fin, int[] arreglo) {
+        this.inicio = inicio;
+        this.fin = fin;
+        this.arreglo = arreglo;
+
+    }
+
+    //Punto de entrada de hilo.
+    public void run() {        
+        System.out.println(this.getName() + " iniciando.");
+        try {
+            for (int i = inicio; i <= fin; i++) {
+                Thread.sleep(0);
+                suma = suma + arreglo[i];
+            }
+        } catch (InterruptedException exc) {
+            System.out.println(this.getName() + " interrumpudo.");
+        }
+        System.out.println(this.getName() + " terminado.");
+    }
+
+    public int getSuma() {
+        return suma;
+    }
+}
